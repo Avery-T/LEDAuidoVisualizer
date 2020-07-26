abbf9896a5eaada27b9bde15f48a9b56a53a694e
Defaulti <snipfar.1@gmail.com>
2020-07-26 14:31:36 -0700

    Now works for one color

--- ledControl/ledControl.ino
+++ ledControl/ledControl.ino
@@ -1,34 +1,41 @@
-#include <Adafruit_NeoPixel.h> 
-#ifdef __AVR__ 
-#endif
+#include <FastLED.h> 
 
-#define NUMPIXELS 60
-#define PIN 6 
-Adafruit_NeoPixel pixels(NUMPIXELS,PIN, NEO_GRB + NEO_KHZ800); 
+#define NUM_LEDS 60
+#define DATA_PIN 6
+#define updateLEDS 1
 
-void setup() 
-{
-  pixels.begin(); 
-  Serial.begin(9600); 
-}
-
-void loop() 
-{
+CRGB leds[NUM_LEDS];
+const unsigned long ledShiftTime = 10000000;
+unsigned int dottt= 0; 
+unsigned int dot = 0;
+unsigned int dot2 = 0; 
+unsigned long previousTime1 = 0; 
+unsigned long previousTime2 = 0;  
   if(Serial.read() > 0)
   {
-    if(Serial.read() > 70)
-    {
-      pixels.fill((255,0,0),0,50);
-      pixels.show();
-    }
-    else
-    {
-      pixels.fill((0,0,255), 0, 50);
-      pixels.show();  
-    }
-    
-  }
-  pixels.clear(); 
-  pixels.show();
+     for(int i = 0; i < updateLEDS; i++)
+     {
+       leds[i] = CRGB(160,32,240);
+     }
   
+    FastLED.show();   
+  }
+  else
+  {
+    for(int i = 0; i < updateLEDS; i++)
+     {
+       leds[i] = CRGB(0,0,0);
+     }
+  
+    FastLED.show(); 
+  }
+  //always shifts the pixels by updateLEDs even when no color
+  
+  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
+  {
+    leds[i] = leds[i - updateLEDS];
+  }
+  FastLED.show(); 
+  
+   
 }
