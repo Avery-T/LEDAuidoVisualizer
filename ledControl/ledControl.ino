fe87620acb06ec8c9bc6ff17f8eb2cb632c26e97
Avery <avery.taylor00@gmail.com>
2020-10-01 05:21:21 -0700

    formated

--- ledControl/ledControl.ino
+++ ledControl/ledControl.ino
@@ -10,32 +10,34 @@ unsigned int dottt= 0;
 unsigned int dot = 0;
 unsigned int dot2 = 0; 
 unsigned long previousTime1 = 0; 
-unsigned long previousTime2 = 0;  
-  if(Serial.read() > 0)
+unsigned long previousTime2 = 0; 
+ 
+if(Serial.read() > 0)
+{
+  for(int i = 0; i < updateLEDS; i++)
   {
-     for(int i = 0; i < updateLEDS; i++)
-     {
-       leds[i] = CRGB(160,32,240);
-     }
-  
-    FastLED.show();   
+    leds[i] = CRGB(160,32,240);
   }
-  else
+  FastLED.show();   
+}
+
+else
+{
+  for(int i = 0; i < updateLEDS; i++)
   {
-    for(int i = 0; i < updateLEDS; i++)
-     {
-       leds[i] = CRGB(0,0,0);
-     }
-  
-    FastLED.show(); 
+    leds[i] = CRGB(0,0,0);
   }
+  
+  FastLED.show(); 
+}
   //always shifts the pixels by updateLEDs even when no color
   
-  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
-  {
-    leds[i] = leds[i - updateLEDS];
-  }
-  FastLED.show(); 
+for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
+{
+  leds[i] = leds[i - updateLEDS];
+}
+
+FastLED.show(); 
   
    
-}
+
