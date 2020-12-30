4b148b6caa0d3eb7bc7160d9bd43dec2a0210091
Avery <snipfar.1@gmail.com>
2020-12-30 05:40:42 -0800

    added a setup and main loop

--- ledControl/ledControl.ino
+++ ledControl/ledControl.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h> 
 
-#define NUM_LEDS 60
+#define NUM_LEDS 300
 #define DATA_PIN 6
 #define updateLEDS 1
 
@@ -11,33 +11,39 @@ unsigned int dot = 0;
 unsigned int dot2 = 0; 
 unsigned long previousTime1 = 0; 
 unsigned long previousTime2 = 0; 
- 
-if(Serial.read() > 0)
-{
-  for(int i = 0; i < updateLEDS; i++)
-  {
-    leds[i] = CRGB(160,32,240);
-  }
-  FastLED.show();   
-}
 
-else
+void setup()
 {
-  for(int i = 0; i < updateLEDS; i++)
-  {
-    leds[i] = CRGB(0,0,0);
-  }
+  FastLED.addLeds<WS2811,DATA_PIN>(leds, NUM_LEDS);
+  Serial.begin(115200); 
   
+  FastLED.clear(); 
   FastLED.show(); 
 }
-  //always shifts the pixels by updateLEDs even when no color
-  
-for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
-{
-  leds[i] = leds[i - updateLEDS];
-}
 
-FastLED.show(); 
+void loop()
+{ 
+  if(Serial.read() > 0)
+  {
+    for(int i = 0; i < updateLEDS; i++)
+    {
+      leds[i] = CRGB(0,255,0);
+    }
+  }    
+  else
+  {
+    for(int i = 0; i < updateLEDS; i++)
+    {
+      leds[i] = CRGB(0,0,0);
+    } 
+  }
+  FastLED.show(); 
+  //always shifts the pixels by updateLEDs even when no color
+  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
+  {
+    leds[i] = leds[i - updateLEDS];
+  }
+  FastLED.show(); 
+} 
   
    
-
