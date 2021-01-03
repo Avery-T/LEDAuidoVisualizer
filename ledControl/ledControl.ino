8470b242efa31571b889fd906387f0603e968080
Avery <snipfar.1@gmail.com>
2021-01-03 02:37:36 -0800

    got rid of redundant variables

--- ledControl/ledControl.ino
+++ ledControl/ledControl.ino
@@ -2,21 +2,14 @@
 
 #define NUM_LEDS 300
 #define DATA_PIN 6
-#define updateLEDS 1
+#define AMTOFLEDS 1
 
 CRGB leds[NUM_LEDS];
-const unsigned long ledShiftTime = 10000000;
-unsigned int dottt= 0; 
-unsigned int dot = 0;
-unsigned int dot2 = 0; 
-unsigned long previousTime1 = 0; 
-unsigned long previousTime2 = 0; 
 
 void setup()
 {
   FastLED.addLeds<WS2811,DATA_PIN>(leds, NUM_LEDS);
-  Serial.begin(115200); 
-  
+  Serial.begin(9600); 
   FastLED.clear(); 
   FastLED.show(); 
 }
@@ -25,25 +18,24 @@ void loop()
 { 
   if(Serial.read() > 0)
   {
-    for(int i = 0; i < updateLEDS; i++)
+    for(int i = 0; i < AMTOFLEDS; i++)
     {
       leds[i] = CRGB(0,255,0);
     }
   }    
   else
   {
-    for(int i = 0; i < updateLEDS; i++)
+    for(int i = 0; i < AMTOFLEDS; i++)
     {
       leds[i] = CRGB(0,0,0);
     } 
   }
   FastLED.show(); 
-  //always shifts the pixels by updateLEDs even when no color
-  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) 
+  //always shifts the pixels
+  for(int i = NUM_LEDS - 1; i >= AMTOFLEDS; i--) 
   {
-    leds[i] = leds[i - updateLEDS];
+    leds[i] = leds[i - AMTOFLEDS];
   }
   FastLED.show(); 
 } 
   
-   
